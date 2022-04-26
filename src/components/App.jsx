6dcac375a9c2bd071f68b8cbdf9ad9e5ef73fe8c
puerarongo <akhardin96@gmail.com>
2022-04-26 16:44:55 +0300

    add fetchFunc in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './navigation/Navigation';
 import HomePage from './homePage/HomePage';
 import MoviesPage from './moviesPage/MoviesPage';
 import MovieDetailsPage from './movieDetailsPage/MovieDetailsPage';
 
+import { searchedFilms } from 'api/movieSearcher';
+
 
 const App = () => {
+  const [request, setRequest] = useState("");
+  const [searchFilm, setSearchFilm] = useState([]);
+
+  useEffect(() => {
+        if (!request) {
+            return
+        }
+        //console.log(request)
+
+        searchedFilms(request).then(response => {
+            const found = response.data.results.map(({ title, id }) => {
+                return { title: title, id: id }
+            })
+            setSearchFilm([...found]);
+        }).catch(error => console.log(error));
+    }, [request]);
+
+  const requestHandler = (request) => {
+    setRequest(request)
+  };
+
+
   return (
     <>
       <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies" element={<MoviesPage submit={requestHandler} films={searchFilm}/>} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />}/>
       </Routes>
     </>
--- src/components/moviesPage/MoviesPage.jsx
+++ src/components/moviesPage/MoviesPage.jsx
@@ -1,41 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import { searchedFilms } from 'api/movieSearcher';
 //import styles from "./MoviesPage.module.css";
 
-const MoviesPage = () => {
+const MoviesPage = ({submit, films}) => {
     const [value, setValue] = useState("");
-    const [request, setRequest] = useState("");
-    const [searchFilm, setSearchFilm] = useState([]);
 
     // const match = useMatch("/movies").pattern.path
     // console.log(match)
-
-
-    useEffect(() => {
-        if (!request) {
-            return
-        }
-        //console.log(request)
-
-        searchedFilms(request).then(response => {
-            const found = response.data.results.map(({ title, id }) => {
-                return { title: title, id: id }
-            })
-            setSearchFilm([...found]);
-        }).catch(error => console.log(error));
-    }, [request]);
-
 
     const inputHandler = e => {
         const { value } = e.currentTarget;
         setValue(value);
     };
 
-
     const submitHandler = (e) => {
         e.preventDefault();
-        setRequest(value);
+        submit(value);
         setValue("");
     };
 
@@ -49,8 +29,8 @@ const MoviesPage = () => {
                 <button type="submit">Search</button>
             </form>
             <ul>
-                {searchFilm.length > 0 && (
-                    searchFilm.map(({ id, title }) => {
+                {films.length > 0 && (
+                    films.map(({ id, title }) => {
                         return <li key={id}>
                             <Link to={`/movies/${id}`} >{title}</Link>
                         </li>
