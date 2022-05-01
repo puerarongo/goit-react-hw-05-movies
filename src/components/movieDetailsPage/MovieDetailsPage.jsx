704b1bcfcbc2d23619868cae94274f68844465ac
puerarongo <akhardin96@gmail.com>
2022-05-01 18:31:13 +0300

    warning 5

--- src/components/movieDetailsPage/MovieDetailsPage.jsx
+++ src/components/movieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Outlet, useParams, useNavigate, useLocation } from "react-router-dom";
+import { Outlet, useParams, useNavigate } from "react-router-dom";
 
 import styles from "./MovieDetailsPage.module.css";
 import Button from './button/Button';
