e82978e9f3928efa1ea9badc99ee60a3cf4a8d19
puerarongo <akhardin96@gmail.com>
2022-05-26 18:05:30 +0300

    add PropTypes

--- src/components/movieDetailsPage/button/Button.jsx
+++ src/components/movieDetailsPage/button/Button.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import styles from "./Button.module.css";
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
 
-const Button = ({handler}) => {
-    return (
-        <div className={styles.container__button}>
-            <button className={styles.button} type='button' onClick={handler}>Go back</button> 
-        </div>
-    )
+const Button = ({ handler }) => {
+  return (
+    <div className={styles.container__button}>
+      <button className={styles.button} type="button" onClick={handler}>
+        Go back
+      </button>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  handler: PropTypes.func.isRequired,
 };
 
 export default Button;
--- src/components/movieDetailsPage/description/Description.jsx
+++ src/components/movieDetailsPage/description/Description.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import styles from "./Description.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Description.module.css';
 
-const Description = ({title, score, overview, genres }) => {
-    return (
-        <div className={styles.description}>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.text}>User score: {score}</p>
-            <h2 className={styles.subtitle}>Overview</h2>
-            <p className={styles.text}>{overview}</p>
-            <h2 className={styles.subtitle}>Genres</h2>
-            <p className={styles.text}>{genres && genres.map(({ name }) => name + " ")}</p>
-        </div>
-    )
+const Description = ({ title, score, overview, genres }) => {
+  return (
+    <div className={styles.description}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.text}>User score: {score}</p>
+      <h2 className={styles.subtitle}>Overview</h2>
+      <p className={styles.text}>{overview}</p>
+      <h2 className={styles.subtitle}>Genres</h2>
+      <p className={styles.text}>
+        {genres && genres.map(({ name }) => name + ' ')}
+      </p>
+    </div>
+  );
+};
+
+Description.propTypes = {
+  title: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  overview: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
 };
 
 export default Description;
